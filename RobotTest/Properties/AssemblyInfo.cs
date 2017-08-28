29e1a93663738ab9ec54afa79c087976068c722e
Mauro Restuccia <mrestuccia@mac.com>
2017-08-28 17:01:38 -0400

    Feat: basic unit test added

---
+++ RobotTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RobotTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RobotTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("48f84dea-d066-49dc-b4b8-349c690a7837")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RobotTest/UnitTest1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program;
+
+namespace RobotTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void Dimensions()
+        {
+            World myWorld;
+
+            // Check Dimension of the world should exit
+            try
+            {
+                myWorld = new World_Mapping(" ").Create();
+            }catch(Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Issue mapping the planet dimensions");
+            }
+
+            // Check empty worlds are not created
+            try
+            {
+                myWorld = new World_Mapping("0 0").Create();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "World need numbers greater than 0");
+            }
+
+            // Check dimensions does not exceed 50
+            try
+            {
+                myWorld = new World_Mapping("60 60").Create();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "World cannot be larger than 50x50");
+            }
+
+
+        }
+
+
+        [TestMethod]
+        public void RobotRotation()
+        {
+            World myWorld = new World_Mapping("2 2").Create();
+
+            // Robot One Right
+            Robot myRobot = new Robot(0, 0, 'N', "R");
+            myRobot.ExecuteRobot(myWorld);
+            Assert.AreEqual(myRobot.Orientation, 'E');
+
+            // Robot 2 right
+            myRobot = new Robot(0, 0, 'N', "RR");
+            myRobot.ExecuteRobot(myWorld);
+            Assert.AreEqual(myRobot.Orientation, 'S');
+
+            // Robot 3 right
+            myRobot = new Robot(0, 0, 'N', "RRR");
+            myRobot.ExecuteRobot(myWorld);
+            Assert.AreEqual(myRobot.Orientation, 'W');
+
+            // Robot Full Spin
+            myRobot = new Robot(0, 0, 'N', "RRRR");
+            myRobot.ExecuteRobot(myWorld);
+            Assert.AreEqual(myRobot.Orientation, 'N');
+
+
+
+        }
+
+
+        [TestMethod]
+        public void RobotMove()
+        {
+            World myWorld = new World_Mapping("2 2").Create();
+
+            // Robot One Right
+            Robot myRobot = new Robot(0, 0, 'N', "FFFF");
+            myRobot.ExecuteRobot(myWorld);
+            Assert.AreEqual(myRobot.IsLost, true);
+
+            // The Robot left scent
+            Assert.AreEqual(myWorld.scents.ToArray()[0], new Location(0,2,'N'));
+
+            // Next robot uses scent
+            myRobot = new Robot(0, 0, 'N', "FFFFFRFF");
+            myRobot.ExecuteRobot(myWorld);
+            Assert.AreEqual(myRobot.X, 2);
+
+        }
+
+    }
+}
