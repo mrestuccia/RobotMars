c8742a677cd38efa14c12741feaf6b90b91f9486
Mauro Restuccia <mrestuccia@mac.com>
2017-08-28 17:23:30 -0400

    Fix: tabs

--- Robot/Classes/World.cs
+++ Robot/Classes/World.cs
@@ -5,23 +5,23 @@ namespace Program
 {
 	public class World
 	{
-		// Properties
-		public int width { get; set; }
-		public int height { get; set; }
+        // Properties
+        public int width { get; set; }
+        public int height { get; set; }
         public Queue<Location> scents { get; set; } 
 
-		// Constructor
-		public World(int width, int height)
-		{
+        // Constructor
+        public World(int width, int height)
+        {
             this.width = width;
             this.height = height;
             this.scents = new Queue<Location>();
-		}
+        }
 
         // Methods
         public bool OutOfRange(Robot robot){
             return ((robot.X < 0) || (robot.Y < 0) || (robot.X > this.width) || (robot.Y > this.height));
-		}
+        }
 
         public void Spray(Location scent){
             this.scents.Enqueue(scent);
@@ -29,10 +29,8 @@ namespace Program
 
         public bool IsSpreayed(Location input)
         {
-		    return this.scents.Contains(input);  
+            return this.scents.Contains(input);  
         }
-
-
 
 	}
 }
